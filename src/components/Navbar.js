e2973c1842c7da44c9158f6be858cf5d6e4beff5
Faizan Alam <faizanalam416@gmail.com>
2024-12-27 20:56:27 +0530

    Design update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,18 +10,18 @@ const Navbar = () => {
 
     return (
         <div className='mx-2'>
-            <div className='flex justify-center md:justify-between sm:py-4 sm:px-20 sticky h-[10%] shadow-md flex-col md:flex-row items-center'>
+            <div className='flex justify-center md:justify-between py-4 px-20 sticky h-[10%] shadow-md flex-col md:flex-row items-center'>
                 <div className='flex items-center p-2 rounded-md gap-2 justify-center md:justify-start'>
-                    <img src={logo} className='w-[4%]'/>
+                    <img src={logo} className='sm:w-[4%] w-[10%]'/>
                     <span className="text-xl font-bold text-[rgb(255,255,255)]">TubeNoter</span>
                 </div>
 
                 <div className='gap-4 flex items-center'>
-                    <button className="flex items-center space-x-1 px-4 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition-colors">
+                    <button className="flex items-center space-x-1 px-3 py-1 md:px-4 md:py-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition-colors">
                         <IoDownloadOutline className='text-lg'/>
                         <span>Download</span>
                     </button>
-                    <button className='flex items-center space-x-1 px-4 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 transition-colors'>
+                    <button className='flex items-center space-x-1 px-3 py-1 md:px-4 md:py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 transition-colors'>
                         <Link onClick={handleContact}>
                             <span className='justify-center flex gap-1 items-center p-[1px]'>
                                 <MdOutlineLocalPostOffice className='text-lg'/>
